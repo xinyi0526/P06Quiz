4cb9285cf3fdfdef28a9cb8da9cd389fa5156276
xinyi0526 <xinyi.luvand620@gmail.com>
2019-05-24 15:32:44 +0800

    Initial commit

--- app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
+++ app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //Check in by 123
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
